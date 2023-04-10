0e5fa76a0510c7498ec9e866180109d9d525de52
RickyAnchores <ricky23anchores@gmail.com>
2023-04-10 12:32:04 +0100

    setup anime api

--- src/Components/AnimeApi/AnimeApi.jsx
+++ src/Components/AnimeApi/AnimeApi.jsx
@@ -6,12 +6,21 @@ const AnimeApi = () => {
 
 
     const fetchData = async () => {
-        let url = ""
+
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': 'bc22f11f37mshcbe9e742a1ed8cap1c4dbcjsna5ccd20c55bc',
+          'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
+        }
+      };
+
+      let url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc'
         try{
-          let resolve = await fetch(url)
+          let resolve = await fetch(url,options)
           let data = await resolve.json()
-          console.log(setData)
-          return setData
+          console.log(data.data[0])
+          setData(data.data[0])
         }
         catch(error){
           console.log("Something went wrong",error)
@@ -20,20 +29,20 @@ const AnimeApi = () => {
 
     const renderData = async () => {
         let data = await fetchData()
-        let content = `<p>${data}</p>`
-
     }
-
-
-
     useEffect(() => {
-      renderData()
+      fetchData()
     })
 
   return (
     <div className='AnimeApi'>
         <h3>AnimeApi</h3>
-
+        <button className="btn" onClick={renderData}>
+          Click
+        </button>
+        <div className="result">
+          <h1>{data.title}</h1>
+        </div>
     </div>
   )
 }
